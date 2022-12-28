a4cba85f741d0b5c64c926e6fe4e5ba853e45ccf
satinder <satindersingh472@gmail.com>
2022-12-27 18:32:36 -0700

    working_on_array_list

--- src/toDos.java
+++ src/toDos.java
@@ -2,7 +2,16 @@ import java.util.ArrayList;
 
 class toDos {
     public static void main(String[] args){
-        ArrayList<String> toDoList = new ArrayList<String>();
+        ArrayList<String> name_list = new ArrayList<String>();
+        String first = "My";
+        String second = "Name";
+        String third = "is";
+        String four = "Satinder";
 
+        name_list.add(first);
+        name_list.add(second);
+        name_list.add(third);
+        name_list.add(four);
+        System.out.println(name_list);
     }
 }
